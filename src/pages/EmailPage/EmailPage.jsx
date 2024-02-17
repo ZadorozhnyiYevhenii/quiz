afc768224667398e1c90fafb3c9970a17e29b1d6
Yevhenii <zheki2004@gmail.com>
2024-02-18 01:52:27 +0200

    deploy: dist folder

--- src/pages/EmailPage/EmailPage.jsx
+++ src/pages/EmailPage/EmailPage.jsx
@@ -1,9 +1,15 @@
-import { QuizTitle } from "../../components/QuizTitle/QuizTitle"
+import { useTranslation } from "react-i18next";
+import { QuizTitle } from "../../components/QuizTitle/QuizTitle";
 
 export const EmailPage = () => {
+  const { t } = useTranslation();
+
   return (
     <main>
-      <QuizTitle title={'Email'} />
+      <QuizTitle
+        title={t("email-page.title")}
+        description={t("email-page.description")}
+      />
     </main>
-  )
-}
+  );
+};
